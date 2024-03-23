501b21b35a775c943e4529b07b9e6f29b859a97d
Zinebb12 <zinebzaid@gmail.com>
2024-03-22 18:26:39 -0700

    test

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,1 +1,13 @@
+# 1-install_a_package.pp
 
+# Ensure the Flask package is installed with version 2.1.0
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+# Install Werkzeug as required 
+package { 'Werkzeug':
+    ensure   => '2.1.1'
+    provider => 'pip3'
+    }
