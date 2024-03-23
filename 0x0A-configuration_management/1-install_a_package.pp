f6e2da6011d27615281aadd2f9d16be00a14f2ff
Zinebb12 <zinebzaid@gmail.com>
2024-03-22 19:29:14 -0700

    test end

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
-# Using Puppet, install puppet-lint
+# install flask using Puppet
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+exec { 'install_flask':
+    command => '/usr/bin/pip3 install flask==2.1.0',
+    path    => ['/usr/bin', '/bin'],
+    unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+}
+
+exec { 'install_werkzeug':
+    command => '/usr/bin/pip3 install werkzeug==2.1.1',
+    path    => ['/usr/bin', '/bin'],
+    unless  => '/usr/bin/pip3 show werkzeug | grep -q "Version: 2.1.1"',
 }
