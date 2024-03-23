f29b9e52a57275aed8038c6f5c623ae283ecae64
Zinebb12 <zinebzaid@gmail.com>
2024-03-22 17:59:54 -0700

    Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# 0-create_a_file.pp
+
+# Ensure a file with the required attributes
+file { '/tmp/school':
+  ensure  => present,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
